9d2b7222957a28f4cdd130bf5c1c3e5b7fed9362
riquerofranco32-prog <riquerofranco32@gmail.com>
2026-04-23 19:11:23 -0300

    Add files via upload

--- pages/montecarlo.js
+++ pages/montecarlo.js
@@ -1,1 +1,11 @@
+import dynamic from 'next/dynamic'
 
+// Carga dinámica para evitar errores de SSR (usa window/Math.random)
+const MonteCarloSentinel = dynamic(
+  () => import('../components/MonteCarloSentinel'),
+  { ssr: false }
+)
+
+export default function MonteCarlo() {
+  return <MonteCarloSentinel />
+}
